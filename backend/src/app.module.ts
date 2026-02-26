82d13891c6001b325ef627bebc5974d4b836b560
ramlow07 <luamramlow@gmail.com>
2026-02-26 13:57:24 -0300

    register application modules in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
@@ -14,6 +17,9 @@ import { AppService } from './app.service';
       }),
       inject: [ConfigService],
     }),
+    ProductsModule,
+    SubscriptionsModule,
+    TransactionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
